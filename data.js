e23ec25592a4d3fbee86252d333dff365572bd1b
AidaS <aida.staugaard@hok.com>
2018-10-03 12:50:28 -0700

    Test

---
+++ data.js
@@ -0,0 +1,65 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-vr-room-1",
+      "name": "VR Room 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 1.541105905772346,
+        "pitch": -0.006593367386271254,
+        "fov": 1.5485123366590539
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-vr-room-2",
+      "name": "VR Room 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 1.781813429413317,
+        "pitch": 0.006702065946910807,
+        "fov": 1.5485123366590539
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
